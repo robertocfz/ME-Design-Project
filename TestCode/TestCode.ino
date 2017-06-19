88dd565a24a1f9ddd4ee7a487a2470f9fadb1e75
Tanbourine <dyztan@gmail.com>
2017-06-19 17:47:29 -0500

    cleaned updateShift

--- TestCode/TestCode.ino
+++ TestCode/TestCode.ino
@@ -5,58 +5,63 @@ const int CLK = 8; //Physical Pin 5
 const int R_Turn = 2; //Physical Pin 11
 const int Brake = 1; //Physical Pin 12
 const int L_Turn = 0; //Physical Pin 13
-unsigned long sigTime = 0;
 
+const int leftHorn = 126;
+const int rightHorn = 504;
+const int leftPer = 14590;
+const int rightPer = 5116;
+const int leftBrake = 1792;
+const int rightBrake = 3074;
 
-int x = 0;
-int foo = 0;
 
 void setup() {
 
-
+  //initialize pins
   pinMode(DATA,OUTPUT);
   pinMode(LATCH,OUTPUT);
   pinMode(CLK,OUTPUT);
   pinMode(R_Turn,INPUT);
   pinMode(Brake,INPUT);
   pinMode(L_Turn,INPUT);
-  updateShift(DATA,CLK,MSBFIRST,0);
-  delay(2000);
+
+  //turn off all leds to signify reset
+  updateShift(0,0);
+  delay(250);
+  updateShift(16383,8191);
+  delay(250);
+  updateShift(0,0);
+  delay(250);
+  updateShift(16383,8191);
+  delay(250);
+  updateShift(0,0);
+  delay(500);
 
 }
 
-
-
-void updateShift(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint16_t val){
-  uint8_t i;
-  //digitalWrite(LATCH,LOW);
-  for(i=0;i<16;i++){
-    if (bitOrder == LSBFIRST){
-      digitalWrite(dataPin, !!(val & (1 << i)));
-    }
-     else{
-       digitalWrite(dataPin, !!(val & (1 << (16 - i))));
-     }
+void updateShift(uint16_t left, uint16_t right){
+  digitalWrite(LATCH,LOW);
   
-      digitalWrite(clockPin, HIGH);
-      digitalWrite(clockPin, LOW);       
+  for(int i=0;i<16;i++){
+    digitalWrite(DATA, !!(right & (1 << (16 - i))));
+    digitalWrite(CLK, HIGH);
+    digitalWrite(CLK, LOW);       
     }
-  //digitalWrite(LATCH,HIGH);
-}
 
+  for(int i=0;i<16;i++){
+    digitalWrite(DATA, !!(left & (1 << (16 - i))));
+    digitalWrite(CLK, HIGH);
+    digitalWrite(CLK, LOW);       
+    }
+    
+   digitalWrite(LATCH,HIGH);
+}
 
 
 void loop() {
-/*
-  for(int i=0;i<x;i++){
-  updateShift(DATA,CLK,MSBFIRST,i);
-  delay(500);
-  }
-  */
-  digitalWrite(LATCH,LOW);
-  updateShift(DATA,CLK,MSBFIRST,8191); //Right side
-  updateShift(DATA,CLK,MSBFIRST,16383);  //Left side 
-  digitalWrite(LATCH,HIGH);
+  //updateShift(126,504); //Left and Right horn
+  updateShift(14590,5116); //Periph
+  //updateShift(1792,3074); //brake lights
+  delay(100);
 }
 
 
