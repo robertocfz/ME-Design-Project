0676c7a7556ce530cac6650c02ea919e6d374d46
Roberto Salinas <robertocfz@utexas.edu>
2017-07-11 13:02:36 -0500

    Added frequency controls to LTBL signal test code.

--- LTBL_Signal_Tests/LTBL_Signal_Tests.ino
+++ LTBL_Signal_Tests/LTBL_Signal_Tests.ino
@@ -10,6 +10,45 @@ int buttonStatus1;
 int buttonStatus2;
 int buttonStatus3;
 
+// Base frequencies are divided by a provided divisor.
+// The base frequency for pins 3, 9, 10, and 11 is 31250 Hz.
+// The base frequency for pins 5 and 6 is 62500 Hz.
+// NOTE: PWM frequencies are tied together in pairs of pins. If one in a
+// pair is changed, the other is also changed to match:
+// Pins 5 and 6 are paired on timer0
+// Pins 9 and 10 are paired on timer1
+// Pins 3 and 11 are paired on timer2
+void setPWMFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x07; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
+
 void setup() {
   Serial.begin(9600);
 
@@ -27,7 +66,10 @@ void loop() {
   buttonStatus1 = digitalRead(buttonPin1);
   buttonStatus2 = digitalRead(buttonPin2);
   buttonStatus3 = digitalRead(buttonPin3);
-
+  
+  setPWMFrequency(pwmOutput1, 31250); // Pin 3, 1 Hz/1 second period
+  setPWMFrequency(pwmOutput2, 62500); // Pin 5, 1 Hz/1 second period
+ 
   if (buttonStatus1 == HIGH) // Left signal PWM output (50% duty cycle) on pin 3
   {
     analogWrite(pwmOutput1, 127);
