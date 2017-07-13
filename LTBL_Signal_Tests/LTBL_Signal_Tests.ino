e17037ad6e5568cf1d46272bfdcc9c03c272ffc7
Roberto Salinas <robertocfz@utexas.edu>
2017-07-12 22:46:56 -0500

    Revised signal testing code and re-organized repo.

--- LTBL_Signal_Tests/LTBL_Signal_Tests.ino
+++ LTBL_Signal_Tests/LTBL_Signal_Tests.ino
@@ -1,94 +1,171 @@
-const int leftIn = 6; // Left signal
-const int rightIn = 7; // Right signal
-const int brakeIn = 8; // Brake signal
+const int leftIn = 7;
+const int rightIn = 8;
+const int brakeIn = 9;
+//const int brakeIn4wire = 9;
+//const int brakeIn5wire = 10;
+const int hazardsIn = 11;
 
-const int leftOut = 3; // Left signal, PWM output
-const int brakeOut = 4; // Brake signal, digital output
-const int rightOut = 5; // Right signal, PWM output
+const int leftOut = 2;
+const int rightOut = 3;
+const int brakeOut = 4;
+//const int brakeOut4wire = 4;
+//const int brakeOut5wire = 5;
+const int hazardsOut = 6;
+
+// OPTIONAL
+//const int hazardsOut1 = 5;
+//const int hazardsOut2 = 6;
+
+unsigned long previousMillisLeft = 0;
+unsigned long previousMillisRight = 0;
+unsigned long previousMillisHazards = 0;
+
+const long interval = 500;
+
+int BRAKE;
+//int BRAKE4wire;
+//int BRAKE5wire;
 
 int LEFT;
-int RIGHT;
-int BRAKE;
+int leftState = LOW;
 
-// Base frequencies are divided by a provided divisor.
-// The base frequency for pins 3, 9, 10, and 11 is 31250 Hz.
-// The base frequency for pins 5 and 6 is 62500 Hz.
-// NOTE: PWM frequencies are tied together in pairs of pins. If one in a
-// pair is changed, the other is also changed to match:
-// Pins 5 and 6 are paired on timer0
-// Pins 9 and 10 are paired on timer1
-// Pins 3 and 11 are paired on timer2
-void setPWMFrequency(int pin, int divisor) {
-  byte mode;
-  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
-    switch(divisor) {
-      case 1: mode = 0x01; break;
-      case 8: mode = 0x02; break;
-      case 64: mode = 0x03; break;
-      case 256: mode = 0x04; break;
-      case 1024: mode = 0x05; break;
-      default: return;
-    }
-    if(pin == 5 || pin == 6) {
-      TCCR0B = TCCR0B & 0b11111000 | mode;
-    } else {
-      TCCR1B = TCCR1B & 0b11111000 | mode;
-    }
-  } else if(pin == 3 || pin == 11) {
-    switch(divisor) {
-      case 1: mode = 0x01; break;
-      case 8: mode = 0x02; break;
-      case 32: mode = 0x03; break;
-      case 64: mode = 0x04; break;
-      case 128: mode = 0x05; break;
-      case 256: mode = 0x06; break;
-      case 1024: mode = 0x07; break;
-      default: return;
-    }
-    TCCR2B = TCCR2B & 0b11111000 | mode;
-  }
+int RIGHT;
+int rightState = LOW;
+
+int HAZARDS;
+int hazardsState = LOW;
+
+void setup() 
+{
+  pinMode(leftIn, INPUT);
+  pinMode(rightIn, INPUT);
+  pinMode(brakeIn, INPUT);
+  //pinMode(brakeIn4wire, INPUT);
+  //pinMode(brakeIn5wire, INPUT);
+  pinMode(hazardsIn, INPUT);
+  
+  pinMode(leftOut, OUTPUT);
+  pinMode(rightIn, INPUT);
+  pinMode(brakeOut, OUTPUT);
+  //pinMode(brakeOut4wire, OUTPUT);
+  //pinMode(brakeOut5wire, OUTPUT);
+  pinMode(hazardsOut, OUTPUT);
+
+  // OPTIONAL
+  //pinMode(hazardsOut1, OUTPUT);
+  //pinMode(hazardsOut2, OUTPUT);
 }
 
-void setup() {
-  //Serial.begin(9600);
-
-  pinMode(leftIn, INPUT); // digital pin 6
-  pinMode(rightIn, INPUT); // digital pin 7
-  pinMode(brakeIn, INPUT); // digital pin 8
-  //pinMode(buttonPin4, INPUT); // digital pin 9
-  
-  pinMode(leftOut, OUTPUT); // digital pin 3
-  pinMode(brakeOut, OUTPUT); // digital pin 4
-  pinMode(rightOut, OUTPUT); // digital pin 5
-  }
-
-void loop() {
+void loop() 
+{
+  RIGHT = digitalRead(rightIn);  
   LEFT = digitalRead(leftIn);
-  RIGHT = digitalRead(rightIn);
+  HAZARDS = digitalRead(hazardsIn);
   BRAKE = digitalRead(brakeIn);
+  //BRAKE4wire = digitalRead(brakeIn4wire);
+  //BRAKE5wire = digitalRead(brakeIn5wire);
   
-  setPWMFrequency(leftOut, 31250); // Pin 3, 1 Hz/1 second period
-  setPWMFrequency(rightOut, 62500); // Pin 5, 1 Hz/1 second period
- 
-  if (LEFT == HIGH) // Left signal PWM output (50% duty cycle) on pin 3
+  unsigned long currentMillis = millis();
+
+  // BRAKE SIGNAL
+  if (BRAKE == HIGH) 
   {
-    analogWrite(leftOut, 127);
-    //delay(1000);
-    //analogWrite(leftOut,0);
+    digitalWrite(brakeOut, HIGH);
   }
-  else if (RIGHT == HIGH) // Right signal PWM output (50% duty cycle) on pin 5
+  else
   {
-    analogWrite(rightOut, 127);
-    //delay(1000);
-    //analogWrite(rightOut,0);
+    digitalWrite(brakeOut, LOW);
   }
-  else if (BRAKE == HIGH) // Brake signal output on pin 4
+
+//  if (BRAKE5wire == HIGH) 
+//  {
+//    digitalWrite(brakeOut5wire, HIGH);
+//  }
+//  else
+//  {
+//    digitalWrite(brakeOut5wire, LOW);
+//  }
+
+//  if (BRAKE4wire == HIGH) 
+//  {
+//    digitalWrite(brakeOut4wire, HIGH);
+//    digitalWrite(leftOut, HIGH);
+//    digitalWrite(rightOut, HIGH);
+//  }
+//  else
+//  {
+//    // Testing needs to be done for this else statement. leftOut and rightOut may not need to be written LOW here
+//    digitalWrite(brakeOut4wire, LOW);
+//    digitalWrite(leftOut, LOW);
+//    digitalWrite(rightOut, LOW);
+//  }
+
+  // LEFT SIGNAL
+  if ( (currentMillis - previousMillisLeft >= interval) && (LEFT == HIGH) ) 
   {
-      while (BRAKE == HIGH)
+    previousMillisLeft = currentMillis;
+  
+    if (leftState == LOW) 
+    {
+      leftState = HIGH;
+    } 
+    else 
+    {
+      leftState = LOW;
+    }
+  digitalWrite(leftOut, leftState);
+  }
+  else if ( (currentMillis - previousMillisLeft >= interval) && (LEFT == LOW) ) 
+  {
+    digitalWrite(leftOut, LOW);
+  }
+
+  // RIGHT SIGNAL
+  if ( (currentMillis - previousMillisRight >= interval) && (RIGHT == HIGH) ) 
+    {
+      previousMillisRight = currentMillis;
+    
+      if (rightState == LOW) 
       {
-        digitalWrite(brakeOut, HIGH);
-        LEFT = digitalRead(leftIn);
+        rightState = HIGH;
+      } 
+      else 
+      {
+        rightState = LOW;
       }
-      digitalWrite(brakeOut, LOW);
+    digitalWrite(rightOut, rightState);
+    }
+  else if ( (currentMillis - previousMillisRight >= interval) && (RIGHT == LOW) ) 
+  {
+    digitalWrite(rightOut, LOW);
   }
+
+  // HAZARDS SIGNAL
+  if ( (currentMillis - previousMillisHazards >= interval) && (HAZARDS == HIGH) ) 
+    {
+      previousMillisHazards = currentMillis;
+    
+      if (hazardsState == LOW) 
+      {
+        hazardsState = HIGH;
+      } 
+      else 
+      {
+        hazardsState = LOW;
+      }
+      digitalWrite(hazardsOut, hazardsState);
+
+      // OPTIONAL
+      //digitalWrite(hazardsOut1, hazardsState);
+      //digitalWrite(hazardsOut2, hazardsState);
+    }
+    else if ( (currentMillis - previousMillisHazards >= interval) && (HAZARDS == LOW) )
+    {
+      digitalWrite(hazardsOut, LOW);
+
+      // OPTIONAL
+      //digitalWrite(hazardsOut1, LOW);
+      //digitalWrite(hazardsOut2, LOW);
+    }
+    
 }
